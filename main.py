2c5795870bfc833d2d229fad88fd789ce672ac35
Nikita Denisov <nikita160412@yandex.ru>
2022-11-19 00:45:17 +0300

    Add dataframe generation

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import pandas as pd
 import numpy as np
+
+libraries = ['TensorFlow', 'PyTorch']
+
+benchmarks = np.zeros(2).reshape(1, 2)
+
+
+def main():
+    df = pd.DataFrame(index=['basic transformer'], columns=libraries, data=benchmarks)
+
+    with open('result.csv', 'w') as f:
+        f.write(df.to_csv())
+
+
+if __name__ == "__main__":
+    main()
