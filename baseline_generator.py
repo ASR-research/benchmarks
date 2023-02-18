6ff4a2324cda6a06ef90d2e8e74e00791c6a1565
Nikita Denisov <nikita160412@yandex.ru>
2023-02-18 13:31:55 +0300

    Fix table generate

--- baseline_generator.py
+++ baseline_generator.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 benchmarks_list = []
 model_name_to_model = {
-    "quartzen": nemo_asr.models.EncDecCTCModel.from_pretrained(model_name="QuartzNet15x5Base-En"),
+    "quartznet": nemo_asr.models.EncDecCTCModel.from_pretrained(model_name="QuartzNet15x5Base-En"),
     "citrinet": nemo_asr.models.EncDecCTCModelBPE.from_pretrained(model_name="stt_en_citrinet_256")
 }
 
@@ -23,7 +23,6 @@ def timeit(func):
         result = func(*args, **kwargs)
         end_time = time.perf_counter()
         total_time = end_time - start_time
-        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
         benchmarks_list.append(total_time)
         return result
     return timeit_wrapper
@@ -33,11 +32,11 @@ def inference(model, files):
     for fname, transcription in zip(files, model.transcribe(paths2audio_files=files)):
         print(f"Audio in {fname} was recognized as: {transcription}")
 
-def get_models():
-    return model_name_to_model.values()
+def get_models(models_name):
+    return [model_name_to_model[model_name] for model_name in models_name]
 
 def get_files():
-    return np.array(['../nemo/LibriSpeech/test-other/1688/142285/1688-142285-0000.flac'])
+    return np.array(['sample.flac'])
 
 def get_benchmarks(num_models, num_files):
     global benchmarks_list
@@ -45,7 +44,8 @@ def get_benchmarks(num_models, num_files):
     return np.reshape(benchmarks_list, (num_models, num_files))
 
 def generate_baseline():
-    models = get_models()
+    models_name = model_name_to_model.keys()
+    models = get_models(models_name)
     files = get_files()
 
     for model in models:
@@ -53,7 +53,7 @@ def generate_baseline():
 
     benchmarks = get_benchmarks(len(models), len(files))
 
-    df = pd.DataFrame(index=model_name_to_model.keys(), columns=files, data=benchmarks)
+    df = pd.DataFrame(index=models_name, columns=files, data=benchmarks)
 
     with open('result.csv', 'w') as f:
         f.write(df.to_csv())
