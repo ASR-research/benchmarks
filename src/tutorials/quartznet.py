2681c9b42fd14c2d0478f2690de779cad5a08630
Nikita Denisov <nikita160412@yandex.ru>
2023-02-24 17:08:20 +0300

    List all available models

--- src/tutorials/quartznet.py
+++ src/tutorials/quartznet.py
@@ -3,13 +3,16 @@ import nemo
 # NeMo's ASR collection - this collections contains complete ASR models and
 # building blocks (modules) for ASR
 import nemo.collections.asr as nemo_asr
+from definitions import ROOT_DIR
 
-# with open('models-quartznet-like.txt', 'w') as f:
-#   f.write(str(nemo_asr.models.EncDecCTCModel.list_available_models()))
+with open(f'{ROOT_DIR}/data/models/models-quartznet-like.txt', 'w') as f:
+  f.write(str(nemo_asr.models.EncDecCTCModel.list_available_models()))
+  # for model in nemo_asr.models.EncDecCTCModel.list_available_models():
+  #   f.write(f'{model["pretrained_model_name"]}\n')
 
 # This line will download pre-trained QuartzNet15x5 model from NVIDIA's NGC cloud and instantiate it for you
 quartznet = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name="QuartzNet15x5Base-En")
 
-files = ['./LibriSpeech/test-other/1688/142285/1688-142285-0000.flac']
+files = [f'{ROOT_DIR}/data/LibriSpeech/sample.flac']
 for fname, transcription in zip(files, quartznet.transcribe(paths2audio_files=files)):
   print(f"Audio in {fname} was recognized as: {transcription}")
